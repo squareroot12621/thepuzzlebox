489d1b18b94a14b5f0c0eca297a0c30e2cdb11e4
squareroot12621 <mathandcoding2718@gmail.com>
2025-03-28 15:54:55 -0600

    Testing...

--- thepuzzlebox.js
+++ thepuzzlebox.js
@@ -3,8 +3,9 @@ const ctx = canvas.getContext("2d");
 var scene = "";
 var loadingStatus = "Setting up...";
 var loadingProgress = 0;
-
+try {
 const startTime = performance.now();
+} catch(e) {throw "OH";}
 function secondsSinceStart() {
     // performance.now() is originally in milliseconds
     return (performance.now() - startTime) / 1000;
